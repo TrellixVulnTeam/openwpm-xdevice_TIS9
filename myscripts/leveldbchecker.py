15e470d4e353d704a5b05556b4078767f5d4fa53
maazbinmusa94@gmail.com <maazbinmusa94@gmail.com>
2021-07-09 17:51:52 -0500

    level db

--- myscripts/leveldbchecker.py
+++ myscripts/leveldbchecker.py
@@ -1,12 +1,13 @@
 import plyvel
+import leveldb
 
 db  = plyvel.DB('../datadir/leveldb')
-
+dbb = leveldb.LevelDB('../datadir/leveldb')
 img = db.get(b'8337212354871836e6763a41e615916c89bac5b3f1f0adf60ba43c7c806e1015')
-
-#img_bytes = img.decode('utf8','ignore').encode('latin-1')
+img1= dbb.Get(b'8337212354871836e6763a41e615916c89bac5b3f1f0adf60ba43c7c806e1015')
+img_bytes = img1.decode()
 
 with open("check.png","wb") as file:
-	file.write(img)
+	file.write(img_bytes)
 
 db.close()
