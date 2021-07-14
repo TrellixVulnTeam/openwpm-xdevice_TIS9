64f095271994c4bf6c97d05e8f7153d2eae2ce6e
maazbinmusa94@gmail.com <maazbinmusa94@gmail.com>
2021-07-14 01:19:53 -0500

    run2 rerun complete

--- myscripts/dbfile.py
+++ myscripts/dbfile.py
@@ -3,11 +3,11 @@ import sqlite3
 import os
 import pandas as pd
 
-def to_csv(path):
+def to_csv(path,path2):
 	if(os.path.exists(path+'/2crawl-data.sqlite') == False):
 		return -1
-	if(os.path.exists(path+'/tables') == False):
-		os.mkdir(path+'/tables')
+	if(os.path.exists(path2+'/tables') == False):
+		os.mkdir(path2+'/tables')
 	db = sqlite3.connect(path+'/2crawl-data.sqlite')
 	cursor = db.cursor()
 	cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -15,9 +15,9 @@ def to_csv(path):
 	for table_name in tables:
 		table_name = table_name[0]
 		table = pd.read_sql_query("SELECT * from %s" % table_name, db)
-		table.to_csv(path+'/tables/'+table_name + '.csv', index_label='index', encoding='utf-8')
+		table.to_csv(path2+'/tables/'+table_name + '.csv', index_label='index', encoding='utf-8')
 	cursor.close()
 	db.close()
 	return 1
 
-to_csv("../tables/run1")
+to_csv("../datadir/","../tables/run2/tables/")
