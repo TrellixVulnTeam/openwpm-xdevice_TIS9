1f9aad4be6e33908bc81672664d097e7b1f9e650
MaazBinMusa <maazbinmusa94@gmail.com>
2021-07-14 01:31:57 -0500

    db not working

--- myscripts/leveldbchecker.py
+++ myscripts/leveldbchecker.py
@@ -1,9 +1,9 @@
 import plyvel
 
-db  = plyvel.DB("../data/run1/leveldb")
+db  = plyvel.DB("./leveldb")
 
 hashes = []
-with open("../tables/run1/tables/Hashes.txt","r") as file:
+with open("../tables/run2/tables/Hashes.txt","r") as file:
 	lines = file.readlines()
 	for line in lines:
 		hashes.append(line.strip('\n').strip('\r'))
@@ -18,7 +18,7 @@ for index,item in enumerate(hashes):
 		print("none")
 		continue
 
-	with open("../tables/run1/images/{}check.png".format(str(index)),"wb") as file:
+	with open("../tables/run2/images/{}check.png".format(str(index)),"wb") as file:
 		file.write(img)
 
 db.close()
